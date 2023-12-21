2b3758d3ea59ddf79e9a12382287e85a3220e119
Mortron <cmsl4y3r@gmail.com>
2023-12-20 19:11:59 -0700

    initial movement no animation

---
+++ PlayerTopDown.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+export var speed := 500 #in pixels per second
+
+var _sprites := {Vector2.RIGHT: 1, Vector2.LEFT: 2, Vector2.UP: 3, Vector2.DOWN: 4}
+var _velocity := Vector2.ZERO
+
+onready var animated_sprite: AnimatedSprite = $AnimatedSprite 
+
+func _physics_process(_delta: float) -> void:
+	var direction := Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	if direction.length() > 1:
+		direction = direction.normalized()
+	move_and_slide(speed * direction)
+ 
+func _unhandled_input(event):
+	if event.is_action_pressed("right"):
+		_update_sprite(Vector2.RIGHT)
+	elif event.is_action_pressed("left"):
+		_update_sprite(Vector2.LEFT)
+	elif event.is_action_pressed("down"):
+		_update_sprite(Vector2.DOWN)
+	elif event.is_action_pressed("up"):
+		_update_sprite(Vector2.UP)
+
+func _update_sprite(direction: Vector2) -> void:
+	animated_sprite.frame = _sprites[direction]
